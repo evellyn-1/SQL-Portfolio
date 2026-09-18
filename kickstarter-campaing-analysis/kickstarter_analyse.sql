65a79c673348fcf8de6b762ee9a48020aed0cd36
evellyn-1 <evelyn.b1@hotmail.com>
2026-09-18 17:00:07 +0200

    Add comment about project failure reasons
    Added a comment questioning the reason for project failures.

---
+++ kickstarter-campaing-analysis/kickstarter_analyse.sql
@@ -0,0 +1,13 @@
+-- Frage: Sind gescheiterte Projekte an fehlenden Unterstützer:innen oder fehlendem Funding gescheitert?
+SELECT main_category, backers, pledged, goal,
+         pledged / goal AS pct_pledged,
+    CASE
+         WHEN pledged / goal >= 1 THEN 'Fully funded'
+         WHEN pledged / goal BETWEEN .75 AND 1 THEN 'Nearly funded'
+         WHEN pledged / goal < .75 THEN 'Not nearly funded'
+         END AS funding_status
+    FROM ksprojects   
+   WHERE state IN ('failed')
+     AND backers >= 100 AND pledged >= 20000
+ORDER BY main_category, pct_pledged DESC
+   LIMIT 10;
